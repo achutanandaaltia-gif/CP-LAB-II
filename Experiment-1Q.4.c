7ac173be86c5f03486ef814f8a8f6c36c0299df5
achutanandaaltia-gif <achutanandaaltia@gmail.com>
2026-02-08 20:35:13 +0530

    Add files via upload

---
+++ Experiment-1Q.4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char vol[100];
+    int i,count=0;
+    printf("enter string\n");
+    gets(vol);
+    for(i=0;vol[i]!=0;i++)
+    if(vol[i]=='a'||vol[i]=='e'||vol[i]=='i'||vol[i]=='o'||vol[i]=='u'||
+        vol[i]=='A'||vol[i]=='E'||vol[i]=='I'||vol[i]=='O'||vol[i]=='u')
+    {
+        count++;
+ 
+    }
+
+    {
+        printf("the volume number is %d\n",count);
+    }
+}
